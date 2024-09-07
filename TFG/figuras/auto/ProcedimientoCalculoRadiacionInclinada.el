ccdd4f4122945ba36ce82b80d7ba7a90fa85be1d
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-09-07 16:37:07 +0200

    Mejoradas figuras

---
+++ TFG/figuras/auto/ProcedimientoCalculoRadiacionInclinada.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "ProcedimientoCalculoRadiacionInclinada"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "standalone"
+    "standalone10"
+    "mathpazo"
+    "tikz"))
+ :latex)
+
